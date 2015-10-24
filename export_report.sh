6b3f4ab4201eb9b5f5077ac4f5454aa81acaf915
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2015-10-24 21:53:32 +0200

    Added js to report export script.

--- export_report.sh
+++ export_report.sh
@@ -3,8 +3,12 @@ F=$1
 RES_DEST_DIR=res
 RES_DEST_DIR_ABS=${PWD}/$RES_DEST_DIR
 RES_SRC_DIR=$HOME/.gnucash
-RES_FILES="normalize.css statement-table.css"
-sed -i "s%${RES_SRC_DIR}%res%g" "$F"
+RES_FILES="normalize.css statement-table.css statement-table.js"
+JQPLOT_SRC_DIR=$(readlink -m "$(dirname $(which gnucash))/../share/gnucash/jqplot")
+JQPLOT_FILES="jquery.min.js"
+sed -ri "s%(${RES_SRC_DIR}|${JQPLOT_SRC_DIR})%res%g" "$F"
 mkdir -p $RES_DEST_DIR
 cd $RES_SRC_DIR
 cp $RES_FILES $RES_DEST_DIR_ABS
+cd $JQPLOT_SRC_DIR
+cp $JQPLOT_FILES $RES_DEST_DIR_ABS
